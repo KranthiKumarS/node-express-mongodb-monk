00de64e4b78e27d2724b7f6cbdd9ad2b0b6acc8b
KranthiKumarS <kranthikumarsha@gmail.com>
2017-06-18 23:00:37 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlists', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/addnewuser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;
